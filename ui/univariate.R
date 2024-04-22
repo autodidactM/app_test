355c152756bf2565dfd158fa0e790dd082828428
autodidactM <88673803+autodidactM@users.noreply.github.com>
2024-04-22 13:23:00 +0530

    Update univariate.R

--- ui/univariate.R
+++ ui/univariate.R
@@ -5,7 +5,7 @@ tabPanel("Univariate Models",
                           c("No Selection"="m110","Normal"="m111", "Logit"="m112","Probit"="m113","Multinomial Probit"="m114","Multinomial Logit"="m115","Ordered Probit"="m116","Negative Binomial(Poisson)"="m117","Tobit"="m118","Quantile"="m119")
              ),
                           h2("Bayesian Econometrics: simulations, models and applications to research, teaching and encoding with responsibility"),
-             image
+             #image
            ),
            mainPanel(fluidRow(column(5,file1m),column(2,filech1m),column(2,rb1m)),
                      uiOutput("univariate_help_data"),
