4d6058cfd7475742954c2b299e8ef12852d76fa8
autodidactM <88673803+autodidactM@users.noreply.github.com>
2024-04-22 14:14:11 +0530

    Update BMAGLM.R

--- ui/BMAGLM.R
+++ ui/BMAGLM.R
@@ -4,7 +4,7 @@ tabPanel("Bayesian Model Averaging",
              radioBMA,
              h1(a(em(strong("BEsmarter",style = "color:light blue")),href = "http://www.besmarter-team.org/")),
              h2("Bayesian Econometrics: simulations, models and applications to research, teaching and encoding with responsibility"),
-             image
+             #image
            ),
            mainPanel(CONDBMA)
          )
